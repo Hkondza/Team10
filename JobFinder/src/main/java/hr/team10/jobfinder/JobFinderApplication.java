93cfde30991c76cd997ae55390751e6faea0ff79
Hkondza <hkondza@algebra.hr>
2025-12-10 19:13:59 +0100

    novo

---
+++ JobFinder/src/main/java/hr/team10/jobfinder/JobFinderApplication.java
@@ -0,0 +1,13 @@
+package hr.team10.jobfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobFinderApplication.class, args);
+    }
+
+}
---
+++ JobFinder/src/test/java/hr/team10/jobfinder/JobFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.team10.jobfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
