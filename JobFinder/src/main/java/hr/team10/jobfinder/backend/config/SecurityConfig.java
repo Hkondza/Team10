dae030ed16017999375a4cb18d2ad6e80d6ef59b
Hkondza <hkondza@algebra.hr>
2025-12-19 10:35:37 +0100

    security

--- JobFinder/src/main/java/hr/team10/jobfinder/backend/config/SecurityConfig.java
+++ JobFinder/src/main/java/hr/team10/jobfinder/backend/config/SecurityConfig.java
@@ -2,15 +2,14 @@ package hr.team10.jobfinder.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
@@ -20,4 +19,3 @@ public class SecurityConfig {
         return http.build();
     }
 }
-
