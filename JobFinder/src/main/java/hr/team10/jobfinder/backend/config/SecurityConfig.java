9f2cb1de0ba03f571e077d8265c7b39b54a684be
gabrielsosic <gabrielsosic004@gmail.com>
2026-01-16 14:13:43 +0100

    add CORS configuration za ranjivost

--- JobFinder/src/main/java/hr/team10/jobfinder/backend/config/SecurityConfig.java
+++ JobFinder/src/main/java/hr/team10/jobfinder/backend/config/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -13,7 +18,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 // nema CSRF jer nema frontend tokena
+
                 .csrf(csrf -> csrf.disable())
 
                 // ISKLJUČI spring login page
@@ -57,5 +65,16 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:8080")); // ← SAMO tvoja domena
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
